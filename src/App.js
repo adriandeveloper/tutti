40421597d5baf78dbdd7d626e5aa3904d7353db4
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-07-03 15:44:30 -0500

    adds Login screen as default view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import Home from './screens/Home';
+// import Home from './screens/Home';
+import Login from './screens/Login';
 
 EStyleSheet.build({
+  $primaryBlue: '#008AD8',
+  $primaryPink: '#FF456E',
+  $secondaryPink: '#F60068',
+  $darkBlue: '#373E52',
+  $facebookColor: '#3C539A',
+  $googleColor: '#DB402C',
+  $white: '#fff',
 
+  // toggle to see where the components are laid out
+  // outline: 1,
 });
 
-export default () => <Home />;
+export default () => <Login />;
